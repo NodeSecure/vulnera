894848ba1478f54bbd77d50a1a60787092a31b8f
antoine-coulon <coulon_antoine@outlook.fr>
2022-02-04 15:44:00 +0100

    refactor: extract vulnerabilities formatter to payload/standardize

--- src/strategies/vuln-payload/standardize.js
+++ src/strategies/vuln-payload/standardize.js
@@ -2,8 +2,16 @@
 import { VULN_MAPPERS } from "./mappers.js";
 
 export function standardizeVulnsPayload(strategy, vulns) {
-    if (!VULN_MAPPERS[strategy]) {
-        return [];
-    }
-    return vulns.map(VULN_MAPPERS[strategy]);
+  if (!VULN_MAPPERS[strategy]) {
+    return [];
+  }
+
+  return vulns.map(VULN_MAPPERS[strategy]);
 }
+
+export function formatVulnerabilities(strategy, vulnerabilities, useStandardFormat) {
+  return useStandardFormat ? standardizeVulnsPayload(
+    strategy, vulnerabilities
+  ) : vulnerabilities;
+}
+

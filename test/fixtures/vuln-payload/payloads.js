2ba7c93c595abaf6e6cfc9e25f04f145db08e519
antoine-coulon <coulon_antoine@outlook.fr>
2022-02-04 15:42:38 +0100

    test: add Sonatype unit and API integration testing

--- test/fixtures/vuln-payload/payloads.js
+++ test/fixtures/vuln-payload/payloads.js
@@ -1,68 +1,94 @@
-import { SNYK_VULNERABILITY, NPM_VULNERABILITY, SECURITYWG_VULNERABILITY } from "./vulns.js"
+import { SNYK_VULNERABILITY, NPM_VULNERABILITY, SECURITYWG_VULNERABILITY, SONATYPE_VULNERABILITY } from "./vulns.js";
 
 export const NPM_VULNS_PAYLOADS = {
-    "inputVulnsPayload": {
-        "vulnerabilities": {
-            "slashify": {
-                "via": [NPM_VULNERABILITY]
-            }
-        }
-    },
-    "outputStandardizedPayload": {
-        "id": undefined,
-        "origin": "npm",
-        "package": NPM_VULNERABILITY.name,
-        "title": NPM_VULNERABILITY.title,
-        "url": NPM_VULNERABILITY.url,
-        "severity": "medium",
-        "vulnerableRanges": [NPM_VULNERABILITY.range],
-        "vulnerableVersions": [NPM_VULNERABILITY.vulnerableVersions]
+  inputVulnsPayload: {
+    vulnerabilities: {
+      slashify: {
+        via: [NPM_VULNERABILITY]
+      }
     }
-}
+  },
+  outputStandardizedPayload: {
+    id: undefined,
+    origin: "npm",
+    package: NPM_VULNERABILITY.name,
+    title: NPM_VULNERABILITY.title,
+    url: NPM_VULNERABILITY.url,
+    severity: "medium",
+    vulnerableRanges: [NPM_VULNERABILITY.range],
+    vulnerableVersions: [NPM_VULNERABILITY.vulnerableVersions]
+  }
+};
 
 export const SNYK_VULNS_PAYLOADS = {
-    "inputVulnsPayload": {
-        "vulnerabilities": [
-            SNYK_VULNERABILITY
-        ],
-    },
-    "outputStandardizedPayload": {
-        "id": SNYK_VULNERABILITY.id,
-        "origin": "snyk",
-        "package": SNYK_VULNERABILITY.package,
-        "title": SNYK_VULNERABILITY.title,
-        "url": SNYK_VULNERABILITY.url,
-        "description": SNYK_VULNERABILITY.description,
-        "severity": SNYK_VULNERABILITY.severity,
-        "vulnerableRanges": SNYK_VULNERABILITY.semver.vulnerable,
-        "vulnerableVersions": [
-            ...SNYK_VULNERABILITY.functions[0].version,
-            ...SNYK_VULNERABILITY.functions[1].version,
-        ],
-        "cves": SNYK_VULNERABILITY.identifiers.CVE,
-        "cvssVector": SNYK_VULNERABILITY.CVSSv3,
-        "cvssScore": SNYK_VULNERABILITY.cvssScore,
-        "patches": SNYK_VULNERABILITY.patches
-    }
-}
+  inputVulnsPayload: {
+    vulnerabilities: [
+      SNYK_VULNERABILITY
+    ]
+  },
+  outputStandardizedPayload: {
+    id: SNYK_VULNERABILITY.id,
+    origin: "snyk",
+    package: SNYK_VULNERABILITY.package,
+    title: SNYK_VULNERABILITY.title,
+    url: SNYK_VULNERABILITY.url,
+    description: SNYK_VULNERABILITY.description,
+    severity: SNYK_VULNERABILITY.severity,
+    vulnerableRanges: SNYK_VULNERABILITY.semver.vulnerable,
+    vulnerableVersions: [
+      ...SNYK_VULNERABILITY.functions[0].version,
+      ...SNYK_VULNERABILITY.functions[1].version
+    ],
+    cves: SNYK_VULNERABILITY.identifiers.CVE,
+    cvssVector: SNYK_VULNERABILITY.CVSSv3,
+    cvssScore: SNYK_VULNERABILITY.cvssScore,
+    patches: SNYK_VULNERABILITY.patches
+  }
+};
 
 export const SECURITYWG_VULNS_PAYLOADS = {
-    "inputVulnsPayload": {
-        "vulnerabilities": [
-            SECURITYWG_VULNERABILITY
-        ],
-    },
-    "outputStandardizedPayload": {
-        "id": SECURITYWG_VULNERABILITY.id,
-        "origin": "node",
-        "package": SECURITYWG_VULNERABILITY.module_name,
-        "title": SECURITYWG_VULNERABILITY.title,
-        "description": SECURITYWG_VULNERABILITY.overview,
-        "vulnerableRanges": [SECURITYWG_VULNERABILITY.vulnerable_versions],
-        "vulnerableVersions": [],
-        "cves": SECURITYWG_VULNERABILITY.cves,
-        "cvssVector": SECURITYWG_VULNERABILITY.cvss_vector,
-        "cvssScore": SECURITYWG_VULNERABILITY.cvss_score,
-        "patchedVersions": SECURITYWG_VULNERABILITY.patched_versions
-    }
-}
+  inputVulnsPayload: {
+    vulnerabilities: [
+      SECURITYWG_VULNERABILITY
+    ]
+  },
+  outputStandardizedPayload: {
+    id: SECURITYWG_VULNERABILITY.id,
+    origin: "node",
+    package: SECURITYWG_VULNERABILITY.module_name,
+    title: SECURITYWG_VULNERABILITY.title,
+    description: SECURITYWG_VULNERABILITY.overview,
+    vulnerableRanges: [SECURITYWG_VULNERABILITY.vulnerable_versions],
+    vulnerableVersions: [],
+    cves: SECURITYWG_VULNERABILITY.cves,
+    cvssVector: SECURITYWG_VULNERABILITY.cvss_vector,
+    cvssScore: SECURITYWG_VULNERABILITY.cvss_score,
+    patchedVersions: SECURITYWG_VULNERABILITY.patched_versions
+  }
+};
+
+export const SONATYPE_VULNS_PAYLOADS = {
+  inputVulnsPayload: {
+    vulnerabilities: [
+      SONATYPE_VULNERABILITY
+    ]
+  },
+  outputStandardizedPayload: {
+    id: SONATYPE_VULNERABILITY.id,
+    origin: "sonatype",
+    /**
+     * Package name is harcoded because the input name is not directly available
+     * from the input vulnerability but from an outer object. Outside of the
+     * context of tests, the package name is provided from a High Order Function
+    */
+    package: "debug",
+    title: SONATYPE_VULNERABILITY.title,
+    url: SONATYPE_VULNERABILITY.reference,
+    description: SONATYPE_VULNERABILITY.description,
+    vulnerableRanges: [],
+    vulnerableVersions: [],
+    cves: [],
+    cvssVector: SONATYPE_VULNERABILITY.cvssVector,
+    cvssScore: SONATYPE_VULNERABILITY.cvssScore
+  }
+};
--- test/strategies/vuln-payload/standardize.js
+++ test/strategies/vuln-payload/standardize.js
@@ -1,37 +1,50 @@
 import test from "tape";
 import { VULN_MODE } from "../../../src/constants.js";
 import { standardizeVulnsPayload } from "../../../src/strategies/vuln-payload/standardize.js";
-import { NPM_VULNS_PAYLOADS, SECURITYWG_VULNS_PAYLOADS, SNYK_VULNS_PAYLOADS } from "../../fixtures/vuln-payload/payloads.js";
+import {
+  NPM_VULNS_PAYLOADS,
+  SECURITYWG_VULNS_PAYLOADS,
+  SNYK_VULNS_PAYLOADS,
+  SONATYPE_VULNS_PAYLOADS
+} from "../../fixtures/vuln-payload/payloads.js";
 
 test("should convert NONE or unknown strategy into blank payload", (tape) => {
-    let notStandardized = standardizeVulnsPayload(VULN_MODE.NONE, [{}, {}]);
-    tape.isEquivalent(notStandardized, []);
-    notStandardized = standardizeVulnsPayload("exploit", {});
-    tape.isEquivalent(notStandardized, []);
-    tape.end();
+  let notStandardized = standardizeVulnsPayload(VULN_MODE.NONE, [{}, {}]);
+  tape.isEquivalent(notStandardized, []);
+  notStandardized = standardizeVulnsPayload("exploit", {});
+  tape.isEquivalent(notStandardized, []);
+  tape.end();
 });
 
 test("should convert NPM strategy vulns payload into NodeSecure standard payload", (tape) => {
-    const { vulnerabilities } = NPM_VULNS_PAYLOADS.inputVulnsPayload;
-    const [fromNPMToStandardFormat] = standardizeVulnsPayload(
-        VULN_MODE.NPM_AUDIT, vulnerabilities.slashify.via
-    );
-    tape.deepEqual(fromNPMToStandardFormat, NPM_VULNS_PAYLOADS.outputStandardizedPayload);
-    tape.end();
+  const { vulnerabilities } = NPM_VULNS_PAYLOADS.inputVulnsPayload;
+  const [fromNPMToStandardFormat] = standardizeVulnsPayload(
+    VULN_MODE.NPM_AUDIT, vulnerabilities.slashify.via
+  );
+  tape.deepEqual(fromNPMToStandardFormat, NPM_VULNS_PAYLOADS.outputStandardizedPayload);
+  tape.end();
 });
 
 test("should convert Snyk strategy payload into NodeSecure standard payload", (tape) => {
-    const [fromSnykToStandardFormat] = standardizeVulnsPayload(
-        VULN_MODE.SNYK, SNYK_VULNS_PAYLOADS.inputVulnsPayload.vulnerabilities
-    );
-    tape.deepEqual(fromSnykToStandardFormat, SNYK_VULNS_PAYLOADS.outputStandardizedPayload);
-    tape.end();
+  const [fromSnykToStandardFormat] = standardizeVulnsPayload(
+    VULN_MODE.SNYK, SNYK_VULNS_PAYLOADS.inputVulnsPayload.vulnerabilities
+  );
+  tape.deepEqual(fromSnykToStandardFormat, SNYK_VULNS_PAYLOADS.outputStandardizedPayload);
+  tape.end();
 });
 
 test("should convert NodeWG strategy payload into NodeSecure standard payload", (tape) => {
-    const [nodeWGToStandardFormat] = standardizeVulnsPayload(
-        VULN_MODE.SECURITY_WG, SECURITYWG_VULNS_PAYLOADS.inputVulnsPayload.vulnerabilities
-    );
-    tape.deepEqual(nodeWGToStandardFormat, SECURITYWG_VULNS_PAYLOADS.outputStandardizedPayload);
-    tape.end();
+  const [nodeWGToStandardFormat] = standardizeVulnsPayload(
+    VULN_MODE.SECURITY_WG, SECURITYWG_VULNS_PAYLOADS.inputVulnsPayload.vulnerabilities
+  );
+  tape.deepEqual(nodeWGToStandardFormat, SECURITYWG_VULNS_PAYLOADS.outputStandardizedPayload);
+  tape.end();
+});
+
+test("should convert Sonatype strategy payload into NodeSecure standard payload", (tape) => {
+  const [sonatypeToStandardFormat] = standardizeVulnsPayload(
+    VULN_MODE.SONATYPE, SONATYPE_VULNS_PAYLOADS.inputVulnsPayload.vulnerabilities
+  );
+  tape.deepEqual(sonatypeToStandardFormat, SONATYPE_VULNS_PAYLOADS.outputStandardizedPayload);
+  tape.end();
 });

821386711fb5f211e857285f1539c10bb6b293e0
antoine-coulon <coulon_antoine@outlook.fr>
2022-04-26 00:51:54 +0200

    test: initialize unit testing boostrapping by using MockAgent

--- test/strategies/sonatype.js
+++ test/strategies/sonatype.js
@@ -1,11 +1,19 @@
 // Import Third-party Dependencies
 import test from "tape";
+import * as httpie from "@myunisoft/httpie";
 
 // Import Internal Dependencies
 import { SonatypeStrategy } from "../../src/strategies/sonatype.js";
-import { SONATYPE_VULNS_PAYLOADS } from "../fixtures/vuln-payload/payloads.js";
 
 // CONSTANTS
+const kSonatypeOrigin = "https://ossindex.sonatype.org";
+const kSonatypeApiPath = "/api/v3/component-report";
+const kHttpClientHeaders = { headers: { "content-type": "application/json" } };
+const kSonatypeVulnComponent = {
+  coordinates: "pkg:npm/fake-npm-package@3.0.1",
+  vulnerabilities: [{ id: "1617", cvssScore: 7.5 }]
+};
+const kFakePackageURL = "pkg:npm/fake-npm-package@3.0.1";
 
 test("SonatypeStrategy definition must return only two keys.", (tape) => {
   const definition = SonatypeStrategy();
@@ -23,11 +31,37 @@ test("SonatypeStrategy definition must return only two keys.", (tape) => {
   tape.end();
 });
 
-test("sonatype strategy: hydratePayloadDependencies using NodeSecure standard format", async(tape) => {
+function setupHttpAgentMock() {
+  const httpDispatcher = httpie.getGlobalDispatcher();
+  const mockedHttpAgent = new httpie.MockAgent();
+
+  mockedHttpAgent.disableNetConnect();
+  httpie.setGlobalDispatcher(mockedHttpAgent);
+
+  return [
+    mockedHttpAgent,
+    () => {
+      mockedHttpAgent.enableNetConnect();
+      httpie.setGlobalDispatcher(httpDispatcher);
+    }
+  ];
+}
+
+test("sonatype strategy: hydratePayloadDependencies", async(tape) => {
   const { hydratePayloadDependencies } = SonatypeStrategy();
   const dependencies = new Map();
+  const [mockedHttpAgent, restoreHttpAgent] = setupHttpAgentMock();
+  const mockedHttpClient = mockedHttpAgent.get(kSonatypeOrigin);
 
-  dependencies.set("debug", {
+  mockedHttpClient
+    .intercept({
+      path: kSonatypeApiPath,
+      method: "POST",
+      body: JSON.stringify({ coordinates: [kFakePackageURL] })
+    })
+    .reply(200, [kSonatypeVulnComponent], kHttpClientHeaders);
+
+  dependencies.set("fake-npm-package", {
     vulnerabilities: [],
     versions: {
       "3.0.1": {
@@ -44,17 +78,36 @@ test("sonatype strategy: hydratePayloadDependencies using NodeSecure standard fo
     1,
     "hydratePayloadDependencies must not add new dependencies by itself"
   );
-  const { vulnerabilities } = dependencies.get("debug");
+
+  const { vulnerabilities } = dependencies.get("fake-npm-package");
   tape.strictEqual(vulnerabilities.length, 1);
 
+  const [fakeNpmPackageVulnerability] = vulnerabilities;
+  tape.deepEqual(fakeNpmPackageVulnerability, {
+    package: "fake-npm-package",
+    id: "1617",
+    cvssScore: 7.5
+  });
+
+  restoreHttpAgent();
   tape.end();
 });
 
-test("sonatype strategy: hydratePayloadDependencies using NodeSecure standard format", async(tape) => {
+test("sonatype strategy: hydratePayloadDependencies when using NodeSecure standard format", async(tape) => {
   const { hydratePayloadDependencies } = SonatypeStrategy();
   const dependencies = new Map();
+  const [mockedHttpAgent, restoreHttpAgent] = setupHttpAgentMock();
+  const mockedHttpClient = mockedHttpAgent.get(kSonatypeOrigin);
 
-  dependencies.set("debug", {
+  mockedHttpClient
+    .intercept({
+      path: kSonatypeApiPath,
+      method: "POST",
+      body: JSON.stringify({ coordinates: [kFakePackageURL] })
+    })
+    .reply(200, [kSonatypeVulnComponent], kHttpClientHeaders);
+
+  dependencies.set("fake-npm-package", {
     vulnerabilities: [],
     versions: {
       "3.0.1": {
@@ -71,15 +124,26 @@ test("sonatype strategy: hydratePayloadDependencies using NodeSecure standard fo
     1,
     "hydratePayloadDependencies must not add new dependencies by itself"
   );
-  const { vulnerabilities } = dependencies.get("debug");
+
+  const { vulnerabilities } = dependencies.get("fake-npm-package");
   tape.strictEqual(vulnerabilities.length, 1);
 
-  const [standardizedVulnFromSonatype] = vulnerabilities;
+  const [{ id, package: packageName, origin, cvssScore }] = vulnerabilities;
 
-  tape.deepEqual(
-    standardizedVulnFromSonatype,
-    SONATYPE_VULNS_PAYLOADS.outputStandardizedPayload
-  );
+  const partialPackageData = {
+    id,
+    package: packageName,
+    origin,
+    cvssScore
+  };
 
+  tape.isEquivalent(partialPackageData, {
+    package: "fake-npm-package",
+    origin: "sonatype",
+    id: "1617",
+    cvssScore: 7.5
+  });
+
+  restoreHttpAgent();
   tape.end();
 });

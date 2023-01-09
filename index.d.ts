5566455d0c172f60a5bcd4d0fd3646f0339154d6
Fabrice <fabnguess@gmail.com>
2023-01-09 17:31:09 +0000

    fix(index.d.ts): add missing .js extension in typeScript definitions

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,21 @@
-import cache from "./types/cache";
+import cache from "./types/cache.js";
 
-import { setStrategy, getStrategy, strategies, defaultStrategyName } from "./types/api";
+import {
+  setStrategy,
+  getStrategy,
+  strategies,
+  defaultStrategyName,
+} from "./types/api.js";
 
-import NpmStrategy from "./types/npm-strategy";
-import NodeStrategy from "./types/node-strategy";
-import SnykStrategy from "./types/snyk-strategy";
-import SonatypeStrategy from "./types/sonatype-strategy";
-import Strategy from "./types/strategy";
+import NpmStrategy from "./types/npm-strategy.js";
+import NodeStrategy from "./types/node-strategy.js";
+import SnykStrategy from "./types/snyk-strategy.js";
+import SonatypeStrategy from "./types/sonatype-strategy.js";
+import Strategy from "./types/strategy.js";
 
 export {
   // Api
   cache,
-
   setStrategy,
   getStrategy,
   strategies,
@@ -22,5 +26,5 @@ export {
   NpmStrategy,
   SnykStrategy,
   SonatypeStrategy,
-  Strategy
+  Strategy,
 };

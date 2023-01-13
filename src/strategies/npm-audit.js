5601488896497ed2112a72c4ad8121996e26d83b
dgouron <damien.gouron@gmail.com>
2023-01-13 13:20:11 +0100

    Work on the pnpm audit

--- src/strategies/npm-audit.js
+++ src/strategies/npm-audit.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable brace-style */
+/* eslint-disable comma-dangle */
 // Import Third-party Dependencies
 import Arborist from "@npmcli/arborist";
+import { audit } from "@pnpm/audit";
 import { getLocalRegistryURL } from "@nodesecure/npm-registry-sdk";
+import fs from "fs";
 
 // Import Internal Dependencies
 import { VULN_MODE, NPM_TOKEN } from "../constants.js";
@@ -14,16 +20,54 @@ export function NPMAuditStrategy() {
   };
 }
 
+async function checkIfProjectUsePnpm(path) {
+  return new Promise((resolve) => {
+    fs.access(`${path}/pnpm-lock.yaml`, fs.constants.F_OK, (err) => {
+      if (err) {
+        resolve(false);
+      } else {
+        console.log("Project use pnpm");
+        console.log("path", path);
+        resolve(true);
+      }
+    });
+  });
+}
+
+async function launchPnpmAudit(path, includeToAudit) {
+  console.log("launch Pnpm Audit");
+
+  const options = {
+    include: { dependancies: true, devDependancies: true, optionDependancies: false },
+  };
+  const lockFile = `${path}/pnpm-lock.yaml`;
+  let auditReport;
+  try {
+    auditReport = await audit(lockFile, {}, options);
+    console.log("Audit report : ", auditReport);
+  } catch (error) {
+    console.log("error pnpm -> ", error);
+  }
+
+  return auditReport.vulnerabilities;
+}
+
+
+
 async function getVulnerabilities(path, options = {}) {
   const { useStandardFormat } = options;
-
+  const isPnpmProject = await checkIfProjectUsePnpm(path);
   const formatVulnerabilities = standardizeVulnsPayload(useStandardFormat);
   const arborist = new Arborist({ ...NPM_TOKEN, path });
-
-  const { vulnerabilities } = (await arborist.audit()).toJSON();
+  const { vulnerabilities } = isPnpmProject
+    ? await launchPnpmAudit(path, ["dependencies", "devDependencies"])
+    : (await arborist.audit()).toJSON();
 
   if (useStandardFormat) {
-    return formatVulnerabilities(VULN_MODE.NPM_AUDIT, Object.values(vulnerabilities));
+    return formatVulnerabilities(
+      VULN_MODE.NPM_AUDIT,
+      Object.values(vulnerabilities)
+    );
   }
 
   return vulnerabilities;
@@ -44,24 +88,43 @@ async function hydratePayloadDependencies(dependencies, options = {}) {
         continue;
       }
 
-      const dependenciesVulnerabilities = dependencies.get(packageName).vulnerabilities;
+      const dependenciesVulnerabilities =
+        dependencies.get(packageName).vulnerabilities;
       dependenciesVulnerabilities.push(
-        ...formatVulnerabilities(
-          VULN_MODE.NPM_AUDIT,
-          [...extractPackageVulnsFromSource(packageVulns)]
-        )
+        ...formatVulnerabilities(VULN_MODE.NPM_AUDIT, [
+          ...extractPackageVulnsFromSource(packageVulns),
+        ])
       );
     }
-  }
-  catch { }
+  } catch {}
 }
 
 function* extractPackageVulnsFromSource(packageVulnerabilities) {
   for (const vulnSource of packageVulnerabilities.via) {
-    const { title, range, id, name, source, url, dependency, severity, version, vulnerableVersions } = vulnSource;
+    const {
+      title,
+      range,
+      id,
+      name,
+      source,
+      url,
+      dependency,
+      severity,
+      version,
+      vulnerableVersions,
+    } = vulnSource;
 
     yield {
-      title, name, source, url, dependency, severity, version, vulnerableVersions, range, id
+      title,
+      name,
+      source,
+      url,
+      dependency,
+      severity,
+      version,
+      vulnerableVersions,
+      range,
+      id,
     };
   }
 }
